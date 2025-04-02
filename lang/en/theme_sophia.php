7210de282e6e8b3c4fa1d5d16f3fcb92c87b398d
reyer3 <56456442+reyer3@users.noreply.github.com>
2025-04-02 09:13:11 -0500

    Agregar cadenas de idioma para el tema

---
+++ lang/en/theme_sophia.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file for theme Sophia.
+ *
+ * @package   theme_sophia
+ * @copyright 2025 OnBotGo
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Cadenas generales del tema.
+$string['pluginname'] = 'Sophia';
+$string['configtitle'] = 'Sophia';
+$string['choosereadme'] = 'Sophia es un tema moderno para Moodle con diseño elegante y enfocado en mejorar la experiencia del aprendizaje.';
+
+// Menú de configuración.
+$string['generalsettings'] = 'Configuración general';
+$string['advancedsettings'] = 'Configuración avanzada';
+
+// Configuración del logotipo.
+$string['logo'] = 'Logotipo';
+$string['logodesc'] = 'Sube un logotipo personalizado para el encabezado del sitio.';
+$string['favicon'] = 'Favicon';
+$string['favicondesc'] = 'Sube un archivo favicon.ico que se mostrará en la pestaña del navegador.';
+
+// Configuración de colores.
+$string['brandcolor'] = 'Color principal';
+$string['brandcolordesc'] = 'Color principal del tema.';
+$string['secondarycolor'] = 'Color secundario';
+$string['secondarycolordesc'] = 'Color secundario usado para resaltar elementos.';
+
+// CSS personalizado.
+$string['customcss'] = 'CSS personalizado';
+$string['customcssdesc'] = 'Agrega tus propias reglas CSS para personalizar aún más el tema.';
+
+// Otros.
+$string['privacy:metadata'] = 'El tema Sophia no almacena ningún dato personal.';
+
+// Mensajes específicos del curso.
+$string['courseprogress'] = 'Progreso del curso';
+$string['activitiescompleted'] = 'Actividades completadas';
+$string['upcomingactivities'] = 'Actividades próximas';
+$string['coursecontent'] = 'Contenido del curso';
+
+// Acciones y botones.
+$string['startcourse'] = 'Comenzar curso';
+$string['continuecourse'] = 'Continuar curso';
+$string['nextmodule'] = 'Siguiente módulo';
+$string['previousmodule'] = 'Módulo anterior';
+$string['completeactivity'] = 'Completar actividad';
